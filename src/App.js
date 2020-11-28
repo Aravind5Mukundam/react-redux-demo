09125ae2cdcdfe736d96d9d1c87194476a82302e
Aravind5Mukundam <mukundamaravind@gmail.com>
2020-11-28 12:17:21 +0530

    logger and playing with mapstate and mapdispatchtoprops

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import store from './redux/store';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import HooksIceCreamContainer from './components/HooksIceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer/>
         <HooksCakeContainer />
         <HooksIceCreamContainer/>
-        {/* <CakeContainer/>
-        <IceCreamContainer/>  */}
+        <CakeContainer/>
+        <IceCreamContainer/> 
+        <NewCakeContainer/>
       </div>
     </Provider>
     
